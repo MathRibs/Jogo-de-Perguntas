30e717f7e176065f7df93858c39ccaa60df2fb4e
MathRibs <matheus2007file@hotmail.com>
2023-12-05 20:34:26 -0300

    Versão Final

---
+++ data/questions.js
@@ -0,0 +1,141 @@
+export default [
+
+    {
+        question: "Qual o melhor professor do UniCeub ?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Todos Juntos",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Meu Objetivo pra hoje foi esse",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Chamadinha Nha Nha",
+            },
+            {
+                id:"3",
+                options:"D",
+                answer:"Hilstown",
+            },
+            {
+                id:"4",
+                options:"D",
+                answer:"Abre o Trello",
+            },
+        ],
+        correctAnswerIndex: 0,
+
+    },
+
+    {
+        question: "Qual e mais poderoso Xamarin ou Android Studio ?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Android Studio",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Windows Forms",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Eclipse",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Topologia Barramento",
+            },
+        ],
+        correctAnswerIndex: 0
+    },
+   
+    {
+        question: "what is 5X5",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"20",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"25",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"10",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"30",
+            },
+        ],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "what is the square root of 169",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"20",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"23",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"13",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"23",
+            },
+        ],
+        correctAnswerIndex: 2
+    },
+    {
+        question: "What is the Smallest Ocean?",
+        options:[
+            {
+                id:"0",
+                options:"A",
+                answer:"Atlantic Ocean",
+            },
+            {
+                id:"1",
+                options:"B",
+                answer:"Pacific Ocean",
+            },
+            {
+                id:"2",
+                options:"C",
+                answer:"Arctic Ocean",
+            },
+            {
+                id:"0",
+                options:"D",
+                answer:"Indian Ocean",
+            },
+        ],
+        correctAnswerIndex: 2
+    }
+]
